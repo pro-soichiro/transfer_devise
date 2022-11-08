14d638604cc5d203ae8f7a2473d5cd94c0552dbd
pro-soichiro <dr.1987626@gmail.com>
2022-11-08 20:34:28 +0900

    初期設定
    * deviseのコントローラー生成
    
    * ルーティングの整理
    
    * usersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,26 @@ Rails.application.routes.draw do
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+    passwords: 'users/passwords',
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    confirmations: 'users/confirmations',
+    unlocks: 'users/unlocks',
+    # omniauth_callbacks: 'users/omniauth_callbacks',
+  }
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy'
   end
 
-  root to: "home#index"
+  authenticated :user do
+    root to: "users/users#index", as: :user_root
+  end
+
+  namespace :users do
+    get 'users/index'
+  end
+
+  root to: 'home#index'
+
 end
